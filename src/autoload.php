bdd39ae60a5b8be8bcf76c94f1f6e62bf2fd5333
Sinri Edogawa <e.joshua.s.e@gmail.com>
2019-05-25 23:33:30 +0800

    more

--- src/autoload.php
+++ src/autoload.php
@@ -1,8 +1,14 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+date_default_timezone_set("Asia/Shanghai");
 
 if (true) {
     $config = [];
     require_once __DIR__ . '/../config.php';
     Ark()->setConfig($config);
+
+//    Ark()->logger("debug")->info("Here is the debug log!");
+//    Ark()->logger("debug")->setIgnoreLevel(LogLevel::DEBUG);
 }
--- src/index.php
+++ src/index.php
@@ -7,35 +7,35 @@ use sinri\bookhub\filter\BookHubMainFilter;
 require_once __DIR__ . '/autoload.php';
 
 $arkWebService = Ark()->webService();
+$router = $arkWebService->getRouter();
+$logger = Ark()->logger("web");
 
-$arkWebService->setDebug(true);
-$arkWebService->setLogger(Ark()->logger("debug"));
-$arkWebService->getRouter()->setDebug(true);
-$arkWebService->getRouter()->setLogger(Ark()->logger("debug"));
+//$arkWebService->setDebug(true);
+//$arkWebService->setLogger($logger);
+//$router->setDebug(true);
+//$router->setLogger($logger);
 
-$arkWebService->getRouter()
-    ->handleRouteError(
-        ArkRouteErrorHandler::buildWithCallback(
-            function ($error_message, $status_code) {
-                Ark()->webOutput()->jsonForAjax(
-                    ArkWebOutput::AJAX_JSON_CODE_FAIL,
-                    ['error' => $error_message, 'code' => $status_code]
-                );
-            }
-        )
-    );
+//$logger->debug(__FILE__ . '@' . __LINE__);
 
-$arkWebService->getRouter()
-    ->loadAllControllersInDirectoryAsCI(
-        __DIR__ . '/controller',
-        'api/',
-        '\sinri\bookhub\controller\\',
-        [BookHubMainFilter::class]
-    );
+$router->setErrorHandler(
+    ArkRouteErrorHandler::buildWithCallback(
+        function ($error_message, $status_code) use ($logger) {
+            $detail = ['error' => $error_message, 'code' => $status_code];
+            $logger->warning(__METHOD__ . '@' . __LINE__, $detail);
+            Ark()->webOutput()->jsonForAjax(ArkWebOutput::AJAX_JSON_CODE_FAIL, $detail);
+        }
+    )
+);
 
-$arkWebService->getRouter()
-    ->get("", function () {
-        echo "Welcome to BookHub!" . PHP_EOL;
-    });
+$router->loadAllControllersInDirectoryAsCI(
+    __DIR__ . '/controller',
+    'api/',
+    '\sinri\bookhub\controller\\',
+    [BookHubMainFilter::class]
+);
+
+$router->get("", function () {
+    echo "Welcome to BookHub!" . PHP_EOL;
+});
 
 $arkWebService->handleRequestForWeb();
